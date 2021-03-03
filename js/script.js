0a04358a3d70adbd9c26d82c41951b48d7c51a4b
Jens <jens.gost@gmail.com>
2021-03-03 15:15:08 +0100

    Images now appear once the user clicks the button

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 "use strict";
 
-   document.querySelector("#choose-button").onclick = async function getPizza() {
+   document.querySelector("#choose-button").onclick = async function (getPizza) {
      let url = new URL('https://foodish-api.herokuapp.com/api/images/pizza');
    
      let response = await fetch(url);
      console.log("we have an answer from the server");
      let URLData = await response.json();
      console.log("we have an image of a pizza");
-
-     console.log(URLData);
+      let hej = "hejsan";
+     console.log(URLData.image);
+     console.log(hej)
 
 
      // ändra i dom när man tycker på knappen
       let parent = document.querySelector("#fetch-target");
-      parent.insertAdjacentHTML("beforeend", `<img src=${URLData}></img>`);
+      parent.insertAdjacentHTML("beforeend", `<img src=${URLData.image} style="width:300px;height:200px;"></img>`);
    
    }
 
