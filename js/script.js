ea57d321f5e5fe994f8a70d543fa7af0271138a6
Jens <jens.gost@gmail.com>
2021-03-03 11:45:41 +0100

    Removed unwanted code

--- js/script.js
+++ js/script.js
@@ -1,57 +1,20 @@
 "use strict";
-/*
-let choose = document.getElementById("choose-button");
 
-choose.addEventListener("submit", () => {
-   let currentVal = choose.nodeValue;
-} )
-*/
-
-
-   async function getPizza() {
+   document.querySelector("#choose-button").onclick = async function getPizza() {
      let url = new URL('https://foodish-api.herokuapp.com/api/images/pizza');
    
      let response = await fetch(url);
+     console.log("we have an answer from the server");
      let URLData = await response.json();
+     console.log("we have an image of a pizza");
 
      console.log(URLData);
 
-     return URLData;
-     
+
+     // ändra i dom när man tycker på knappen
+      let parent = document.querySelector("#fetch-target");
+      parent.insertAdjacentHTML("beforeend", `<img src=${URLData}></img>`);
+   
    }
 
-getPizza().then((URLData) =>{
-   let objectURL = URL.createObjectURL(URLData);
-   let image = document.createElement('img');
-   image.src = objectURL;
-   document.body.appendChild(image);
-}).catch(e => console.log(e));
-
-/*
-      if (response.ok) {
-         return await response.json();
-        } else {
-         console.log("Did not work")
-        }  
-     }
-
-     getPizza().then((pizzaImage) => {
-      let objectURL = URL.createObjectURL(pizzaImage);
-      let image = document.createElement('img');
-      image.src = objectURL;
-      document.body.appendChild(image);
-     }).catch(e => console.log(e));
-
-     */
-
-/*
-fetch('https://foodish-api.herokuapp.com/api/images/pizza').then(res => {
-     if (res.ok){
-        console.log("Success")
-     }
-     else{
-        console.log("No success")
-     }
-    })
-    .then(data => console.log(data));
-    */
+   console.log("script.js has finished running");
